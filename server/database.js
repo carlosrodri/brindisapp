91c2c9d28d480424b0990bbd17ea397563f817ba
carlosrodri <carlos.rodriguez12@uptc.edu.co>
2020-03-07 11:20:12 -0500

    database

--- server/database.js
+++ server/database.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 
 /*'mongodb://localhost/bar'*/ 
-const URI = 'mongodb+srv://admin:abc.52106@cluster0-ekfve.mongodb.net/test?retryWrites=true&w=majority';
-
+const URIM = 'mongodb+srv://admin:abc.52106@cluster0-ekfve.mongodb.net/test?retryWrites=true&w=majority';
+const URI = 'mongodb://admin:abc.52106@SG-brindis-31778.servers.mongodirector.com:51690,SG-brindis-31779.servers.mongodirector.com:51690,SG-brindis-31780.servers.mongodirector.com:51690/admin?replicaSet=RS-brindis-0&ssl=true'
 
 
 /*const MongoClient = require('mongod').MongoClient();
@@ -15,30 +15,12 @@ client.connect(err => {
   client.close();
 });*/
 
-const connectDB = async () => {
-  try {
-  const connection = await mongoose.connect(
-  URI,
-  {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-  }
-  )
-  console.log('MongoDB connected: ${connection.connection.host}');
-  } catch (error) {
-  console.log('MongoDB error when connecting: ${error}');
-  }
-  }
-  connectDB()
 
-
-/*mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then((db) => {
     console.log('DB is connected');
 }).catch((err) => {
     console.error(err);
-});*/
+});
 
 module.exports = mongoose;
