1cb6dd3b590b395d453e3d85ac7125a85c44b99f
carlosrodri <carlos.rodriguez12@uptc.edu.co>
2020-03-07 08:39:12 -0500

    adding

--- server/database.js
+++ server/database.js
@@ -2,13 +2,26 @@ const mongoose = require('mongoose');
 
 
 /*'mongodb://localhost/bar'*/ 
-const URI = 'mongodb+srv://admin:abc.52106@cluster0-5cmep.mongodb.net/test?retryWrites=true&w=majority';
+//const URI = 'mongodb+srv://admin:abc.52106@cluster0-5cmep.mongodb.net/test?retryWrites=true&w=majority';
 
-mongoose.connect(URI)
+
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:abc.52106@cluster0-5cmep.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+/*mongoose.connect(URI)
 .then((db) => {
     console.log('DB is connected');
 }).catch((err) => {
     console.error(err);
-});
+});*/
 
 module.exports = mongoose;
