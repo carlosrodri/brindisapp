b3cd6e0028903d4e5c830c8cb80286f1d8fb8c2f
carlosrodri <carlos.rodriguez12@uptc.edu.co>
2020-03-07 09:21:05 -0500

    adding

--- server/database.js
+++ server/database.js
@@ -17,7 +17,7 @@ client.connect(err => {
 });*/
 
 
-mongoose.connect(URI)
+mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then((db) => {
     console.log('DB is connected');
 }).catch((err) => {
