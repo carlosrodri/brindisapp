f00a616fa04b1a1666800c6fdf50af65f2d37ba9
carlosrodri <carlos.rodriguez12@uptc.edu.co>
2020-03-07 09:52:04 -0500

    adding

--- server/database.js
+++ server/database.js
@@ -7,9 +7,9 @@ const URI = 'mongodb+srv://admin:abc.52106@cluster0-5cmep.mongodb.net/test?retry
 
 
 
-const MongoClient = require('mongod').MongoClient;
+const MongoClient = require('mongod').MongoClient();
 const uri = "mongodb+srv://admin:abc.52106@cluster0-5cmep.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const client = new MongoClient(uri, { useNewUrlParser: true }, { useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
