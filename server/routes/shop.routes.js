74cf534d441cbb8484d16913e8af17756799e442
carlosrodri <carlos.rodriguez12@uptc.edu.co>
2020-03-08 11:51:06 -0500

    adding service shop by id

--- server/routes/shop.routes.js
+++ server/routes/shop.routes.js
@@ -7,7 +7,7 @@ router.get('/', shop.getShops) ;
 router.post('/', shop.createShop);
 router.get('/mail/:mail', shop.getShopByMail);
 router.get('/name/:name', shop.getShopByName);
-router.get('/id/:id', shop.getShopByName);
+router.get('/id/:id', shop.getShopById);
 router.delete('/:id', shop.deleteShop);
 router.put('/:id', shop.editShop);
 
