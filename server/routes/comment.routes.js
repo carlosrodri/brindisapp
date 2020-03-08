961e7b2d37cf476b6cb4a998eacf1de9f2093b24
carlosrodri <carlos.rodriguez12@uptc.edu.co>
2020-03-08 17:05:52 -0500

    adding comments function

--- server/routes/comment.routes.js
+++ server/routes/comment.routes.js
@@ -6,6 +6,7 @@ const comment = require('../controllers/comment.controller');
 router.get('/', comment.getComments) ;
 router.post('/', comment.createComment);
 router.get('/mail/:mail', comment.getCommentbyMail);
+router.get('/shop/:shop', comment.getCommentsByShop)
 router.put('/:id', comment.editComment);
 router.delete('/:id', comment.deleteComment);
 
