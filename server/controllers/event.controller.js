471645ba3f6a96a5c83272d6ebff40f665dc1cbf
carlosrodri <carlos.rodriguez12@uptc.edu.co>
2020-03-12 15:18:26 -0500

    somerthing goes here

--- server/controllers/event.controller.js
+++ server/controllers/event.controller.js
@@ -57,8 +57,7 @@ eventController.getEventsByShop = async (req, res) => {
 
 eventController.getEventsByCity = async (req, res) => {
     console.log(req.params.city + " ciudad que está acá");
-
-    const events = Event.findById( {
+    const events = await Event.findById( {
         city: req.params.city
     })
 
