bcc9ba7e8f1d03e1c720f41e1024acbe3d421e62
carlosrodri <carlos.rodriguez12@uptc.edu.co>
2020-03-29 10:04:38 -0500

    get all attends

--- server/models/attend.js
+++ server/models/attend.js
@@ -3,7 +3,8 @@ const { Schema } = mongoose;
 
 const attendSchema = new Schema({
     user: String,
-    event: String
+    event: String,
+    city: String
 })
 
 module.exports = mongoose.model('Attend', attendSchema)
