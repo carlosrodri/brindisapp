b7a080f470d57288fdcfb850c81ee7a5ba487069
carlosrodri <carlos.rodriguez12@uptc.edu.co>
2020-03-29 09:55:37 -0500

    get all attends

--- server/routes/attend.routes.js
+++ server/routes/attend.routes.js
@@ -5,6 +5,7 @@ const attend = require('../controllers/attend.controller');
 
 router.get('/event/:event', attend.getAttendByEvent) ;
 router.get('/users/:mail', attend.getAttendByUser) ;
+router.get('/', attend.getAttends) ;
 router.post('/', attend.addAttend);
 
 module.exports = router;
