d89a769f2aac075a0ef0e3a8eccbc4ac2ad64c62
carlosrodri <carlos.rodriguez12@uptc.edu.co>
2020-04-09 11:55:34 -0500

    adding match service

--- server/index.js
+++ server/index.js
@@ -64,6 +64,30 @@ app.post('/api/payment', (req, res) => {
         },
         function(err, charge) {
           if (err) {
+            switch (err.type) {
+                case 'StripeCardError':
+                  // A declined card error
+                  err.message; // => e.g. "Your card's expiration year is invalid."
+                  break;
+                case 'StripeRateLimitError':
+                  // Too many requests made to the API too quickly
+                  break;
+                case 'StripeInvalidRequestError':
+                  // Invalid parameters were supplied to Stripe's API
+                  break;
+                case 'StripeAPIError':
+                  // An error occurred internally with Stripe's API
+                  break;
+                case 'StripeConnectionError':
+                  // Some kind of error occurred during the HTTPS communication
+                  break;
+                case 'StripeAuthenticationError':
+                  // You probably used an incorrect API key
+                  break;
+                default:
+                  // Handle any other types of unexpected errors
+                  break;
+              }
               console.log(err + '  error');
           } else {
               console.log(charge + '  charge');
