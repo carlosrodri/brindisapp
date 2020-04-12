d577c21752e2013218609ac4e702d695624b51c7
carlosrodri <carlos.rodriguez12@uptc.edu.co>
2020-04-12 12:21:44 -0500

    adding

--- server/models/event.js
+++ server/models/event.js
@@ -5,6 +5,7 @@ const eventSchema = new Schema({
     date: {type: String, required: true},
     description: {type: String, required: true},
     name: {type: String, required: true},
+    shopImg:{type: String, required: true},
     cover: String,
     shop: {type: String, required: true},
     initHour: {type: String},
