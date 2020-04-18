b8224cddea4b38176d8897654bcacc150f9cbf8a
carlosrodri <carlos.rodriguez12@uptc.edu.co>
2020-04-18 17:42:27 -0500

    payment

--- server/models/payment.js
+++ server/models/payment.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose
 const paymentSchema = new Schema({
     payDate: {type: Date, required: true},
     userMail: {type: String, required: true},
-    defeat: {type: Boolean, required: true}
+    defeat: {type: Boolean, required: true},
+    proof:{type: String, required: true}
 })
 
 module.exports = mongoose.model('Payment', paymentSchema)
